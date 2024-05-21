9f76173da673a84900a81467db47a86b97f99f37
Hatim Selmun <selmun13@gmail.com>
2024-05-21 20:57:14 +0100

    fifo caching system

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""FIFO Caching"""
+from typing import Union
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """A FIFO algorithm caching system
+    """
+    def __init__(self):
+        """Instantiates the class
+        """
+        super().__init__()
+
+    def put(self, key: str, item: str) -> None:
+        """Assigns the item to key in cache_data dict"""
+        if key and item:
+            if len(self.cache_data.items()) > self.MAX_ITEMS:
+                keys = list(self.cache_data.keys())
+                # print(keys)
+                del self.cache_data[keys[0]]
+                print("DISCARD: {}".format(keys[0]))
+            self.cache_data.update({key: item})
+
+    def get(self, key: str) -> Union[str, None]:
+        """Retrives the value linked to key if exists in cache_data
+        """
+        if key and key in self.cache_data.keys():
+            return self.cache_data.get(key)
+        return None
