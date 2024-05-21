7a6a7232fce8b646eb4b2faaba054e4d55fe52e5
Hatim Selmun <selmun13@gmail.com>
2024-05-21 21:08:03 +0100

    lifo caching sys

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""LIFO Caching"""
+from typing import Union
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """A FIFO algorithm caching system
+    """
+    def __init__(self):
+        """Instantiates the class
+        """
+        super().__init__()
+
+    def put(self, key: str, item: str) -> None:
+        """Assigns the item to key in cache_data dict"""
+        if key and item:
+            if len(list(self.cache_data.keys())) == self.MAX_ITEMS:
+                keys = list(self.cache_data.keys())
+                del self.cache_data[keys[-1]]
+                print("DISCARD: {}".format(keys[-1]))
+
+            self.cache_data.update({key: item})
+
+    def get(self, key: str) -> Union[str, None]:
+        """Retrives the value linked to key if exists in cache_data
+        """
+        if key and key in self.cache_data.keys():
+            return self.cache_data.get(key)
+        return None
