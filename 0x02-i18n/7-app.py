1387cd5cb81462a553f2683eab76f266277f9dbe
Hatim Selmun <selmun13@gmail.com>
2024-05-28 19:23:44 +0100

    appropriate timezone

---
+++ 0x02-i18n/7-app.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python3
+"""Basic flask app"""
+from flask import Flask, render_template, request, g
+from flask_babel import Babel, _
+from typing import Optional, Union, Dict
+from pytz import timezone
+
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+class Config:
+    """Configuration class"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+babel = Babel(app)
+
+
+@app.route("/", methods=['GET'])
+def welcome_to_holberton():
+    """The welcome page"""
+    user = getattr(g, 'user', None)
+    return render_template('7-index.html', user=user)
+
+
+@babel.localeselector
+def get_locale() -> Optional[str]:
+    """Determines the best locale"""
+    if request.args:
+        dict_args = dict(request.args)
+        if 'locale' in dict_args and dict_args['locale']\
+                in app.config['LANGUAGES']:
+            return dict_args['locale']
+    user = getattr(g, 'user', None)
+    if user:
+        return user.locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+def get_user() -> Union[Dict, None]:
+    """Getting the user dictionary or None"""
+    if request.args:
+        login_as = request.args.get('login_as')
+        if login_as and login_as.isdigit():
+            return users.get(int(login_as))
+        return None
+
+
+@app.before_request
+def before_request() -> None:
+    """Finds the user if any and sets it as a global
+    on flask.g.user"""
+    g.user = get_user()
+
+
+@babel.timezoneselector
+def get_timezone() -> Union[str, None]:
+    """Getting the appropriate timezone"""
+    if request.args:
+        tz = request.args.get('timezone')
+        if tz:
+            try:
+                timezone(tz)
+                return tz
+            except Exception as e:
+                print(e)
+
+    user = getattr(g, 'user', None)
+    if user:
+        tz = user.timezone
+        try:
+            timezone(tz)
+            return tz
+        except Exception as e:
+            print(e)
+
+    # return app.conig['BABEL_DEFAULT_TIMEZONE']
+
+
+if __name__ == "__main__":
+    app.run()
